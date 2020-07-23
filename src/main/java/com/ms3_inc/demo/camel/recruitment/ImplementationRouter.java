8489dd16a52f0192967db77802b123a3177b3241
Jose Montoya <jam01@protonmail.com>
2020-07-23 13:55:24 -0500

    adds model transformation
    adding the model transformation before completing the implementation to
    highlight camel-datasonnet integration
    
    run
    
    curl --location --request POST 'localhost:9000/api/recruitment-events' \
    --header 'Content-Type: application/json' \
    --data-raw '{
      "date": "2020-07-23T16:04:17.849Z",
      "supervisor": {
        "name": {
          "first": "Recruiter",
          "last": "McRecruiterFace"
        }
      },
      "prospects": [
        {
          "name": {
            "first": "Student 0",
            "last": "McStudentFace 0"
          },
          "college": "fine arts"
        },
        {
          "name": {
            "first": "Student 1",
            "last": "McStudentFace 1"
          },
          "college": "engineering",
          "contact": [
            {
              "type": "personalPhone",
              "value": "1234567890"
            },
            {
              "type": "email",
              "value": "student1@demos.r.us.com"
            }
          ]
        }
      ]
    }'
    
    to see the transformation result in the logs, or optionally remove the
    thank you message transformation to set the model transformation in the
    response

--- src/main/java/com/ms3_inc/demo/camel/recruitment/ImplementationRouter.java
+++ src/main/java/com/ms3_inc/demo/camel/recruitment/ImplementationRouter.java
@@ -8,6 +8,8 @@ public class ImplementationRouter extends RouteBuilder {
     @Override
     public void configure() throws Exception {
         from("direct:POST_recruitment-events")
+                .transform(datasonnet("resource:classpath:recruitment-model.ds"))
+                .log("${body}")
                 .transform(datasonnet("{message: 'thank you!'}"));
     }
 }
